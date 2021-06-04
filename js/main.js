adaff354a1f914bc0274e614a8fd7a834bea06cc
onemorelenin <85076205+onemorelenin@users.noreply.github.com>
2021-06-04 20:29:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper-container', {
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+});
